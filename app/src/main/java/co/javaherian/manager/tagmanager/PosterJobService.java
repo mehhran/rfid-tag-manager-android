526ec54519374f93b215bf0bcd98e5bf24a01231
Mehran J <mehhran@gmail.com>
2022-05-09 19:41:42 -0700

    house keeping

--- app/src/main/java/co/javaherian/manager/tagmanager/PosterJobService.java
+++ app/src/main/java/co/javaherian/manager/tagmanager/PosterJobService.java
@@ -3,29 +3,10 @@ package co.javaherian.manager.tagmanager;
 import android.app.job.JobParameters;
 import android.app.job.JobService;
 import android.util.Log;
-
 import java.io.IOException;
-import java.io.InputStream;
-import java.security.KeyManagementException;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.Objects;
-
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSocketFactory;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.TrustManagerFactory;
-import javax.net.ssl.X509TrustManager;
-
 import okhttp3.Call;
 import okhttp3.FormBody;
-import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
@@ -44,70 +25,49 @@ public class PosterJobService extends JobService {
 
     private void doBackgroundWork(JobParameters params) {
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
+        new Thread(() -> {
 
-                /*
-                *  Something like this will be sent to the server:
-                *
-                *  {
-                *   "tags" : [
-                *   {"strEPC": strEPC, "antenna": antenna, "strRSSI": strRSSI, "nReadCount": nReadCount},
-                *   {"strEPC": strEPC, "antenna": antenna, "strRSSI": strRSSI, "nReadCount": nReadCount},
-                *   {"strEPC": strEPC, "antenna": antenna, "strRSSI": strRSSI, "nReadCount": nReadCount},
-                *   ... ],
-                *   "scan_time": currentTime,
-                *  }
-                *
-                */
+            /*
+            *  Something like this will be sent to the server:
+            *
+            *  {
+            *   "tags" : [
+            *   {"strEPC": strEPC, "antenna": antenna, "strRSSI": strRSSI, "nReadCount": nReadCount},
+            *   {"strEPC": strEPC, "antenna": antenna, "strRSSI": strRSSI, "nReadCount": nReadCount},
+            *   {"strEPC": strEPC, "antenna": antenna, "strRSSI": strRSSI, "nReadCount": nReadCount},
+            *   ... ],
+            *   "scan_time": currentTime,
+            *  }
+            *
+            */
 
-                RequestBody formBody = new FormBody.Builder()
-                        .add("tags", params.getExtras().getString("tags"))
-                        .add("scan_time", params.getExtras().getString("scan_time"))
-                        .build();
+            RequestBody formBody = new FormBody.Builder()
+                    .add("tags", params.getExtras().getString("tags"))
+                    .add("scan_time", params.getExtras().getString("scan_time"))
+                    .build();
 
-                String myApiKey = BuildConfig.API_KEY;
-                String myApiUrl = BuildConfig.TAGS_API_URL;
+            String myApiKey = BuildConfig.API_KEY;
+            String myApiUrl = BuildConfig.TAGS_API_URL;
 
-                Request request = new Request.Builder()
-                        .url(myApiUrl)
-                        .post(formBody)
-                        .addHeader("Authorization", myApiKey)
-                        .build();
+            Request request = new Request.Builder()
+                    .url(myApiUrl)
+                    .post(formBody)
+                    .addHeader("Authorization", myApiKey)
+                    .build();
 
-                OkHttpClient client = new OkHttpClient();
-                Call call = client.newCall(request);
+            OkHttpClient client = new OkHttpClient();
+            Call call = client.newCall(request);
 
-                try {
-                    Response response = call.execute();
-                    Objects.requireNonNull(response.body()).close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-                Log.d(TAG, "Job finished");
-                jobFinished(params, false);
-            }
-        }).start();
-    }
-
-    protected KeyStore myKeyStore() {
-        try {
-            final KeyStore ks = KeyStore.getInstance("BKS");
-
-            final InputStream in = this.getResources().openRawResource(R.raw.manager);
             try {
-                ks.load(in, this.getString( R.string.mystore_password ).toCharArray());
-            } finally {
-                in.close();
+                Response response = call.execute();
+                Objects.requireNonNull(response.body()).close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
 
-            return ks;
-
-        } catch( Exception e ) {
-            throw new RuntimeException(e);
-        }
+            Log.d(TAG, "Job finished");
+            jobFinished(params, false);
+        }).start();
     }
 
     @Override
